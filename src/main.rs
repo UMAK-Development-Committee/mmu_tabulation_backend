e1609e269785c886fd8bf0ac90b7848b29af6655
Gihyun <giordnnuz@gmail.com>
2023-09-16 13:13:26 +0800

    Added Postgres notifications

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ use axum::{
     http::{HeaderValue, Method, StatusCode},
     response::{IntoResponse, Response},
     routing::{get, post},
-    Json, Router,
+    Extension, Json, Router,
 };
 use dotenv::dotenv;
 use futures::{sink::SinkExt, stream::StreamExt};
 use serde::{Deserialize, Serialize};
-use sqlx::PgPool;
+use sqlx::{postgres::PgListener, PgPool};
 use std::{
     collections::HashSet,
     env, io,
@@ -35,11 +35,13 @@ use web::{auth, candidate, category, criteria, event, judge, score};
 
 // Our shared state
 struct AppState {
-    // We require unique usernames. This tracks which usernames have been taken.
+    // Idk if we need to track the judges
     user_set: Mutex<HashSet<String>>,
     // Channel used to send messages to all connected clients.
     tx: broadcast::Sender<String>,
 }
+
+// TODO: Add Postgres listener
 
 #[tokio::main]
 async fn main() {
@@ -51,22 +53,45 @@ async fn main() {
 
     let user_set = Mutex::new(HashSet::new());
     let (tx, _rx) = broadcast::channel(100);
-    let app_state = Arc::new(AppState { user_set, tx });
 
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL env not found.");
-
     let pool = sqlx::postgres::PgPool::connect(&db_url)
         .await
         .expect("Can't connect to database.");
+    let mut pg_listener = PgListener::connect_with(&pool)
+        .await
+        .expect("Can't connect with listener.");
 
-    // TODO: Have relationships between candidates and other stuff, put these in separate
-    // route for now
+    pg_listener.listen_all(vec!["updates"]).await.unwrap();
+
+    let app_state = Arc::new(AppState { user_set, tx });
+    let app_state_clone = Arc::clone(&app_state);
+
+    println!("\nNow listening to Postgres...\n");
+
+    // WEB SOCKET (with db)
+    // When something happens to the database, send it to the websocket for the frontend to be updated in real time
+    tokio::spawn(async move {
+        loop {
+            while let Some(notification) = pg_listener.try_recv().await.unwrap() {
+                let payload = notification.payload();
+                app_state_clone.tx.send(payload.to_string()).unwrap();
+
+                println!("Notification 8000: {payload:?}\n");
+            }
+
+            println!("Connection to Postgres lost.");
+        }
+    });
+
+    // TODO: improve the way the routes are setup if possible
     let app = Router::new()
-        // WEB SOCKET
+        // WEB SOCKET (without db)
         .route("/ws", get(ws_handler))
         .with_state(app_state)
-        // CRUD routes
         .route("/", get(hello_world))
+        // CRUD routes
+        // Auth
         .route("/login", post(auth::login))
         .route("/logout", post(auth::logout))
         // Events
@@ -81,8 +106,6 @@ async fn main() {
             "/events/:event_id/categories/:category_id",
             get(category::get_category),
         )
-        // Judges
-        .route("/judges", post(judge::create_judge))
         // Criterias
         .route(
             "/events/:event_id/categories/:category_id/criterias",
@@ -102,10 +125,8 @@ async fn main() {
             "/candidates/:candidate_id/scores",
             get(score::get_candidate_scores),
         )
-        // .route(
-        //     "/candidates/:candidate_id/add_scores",
-        //     get(score::add_criteria_scores),
-        // )
+        // Judges
+        .route("/judges", post(judge::create_judge))
         .route("/scores", post(score::submit_score))
         .route("/notes", post(candidate::create_note))
         .layer(
@@ -117,7 +138,7 @@ async fn main() {
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
 
-    println!("\nServer has started, listening on: {}\n", addr);
+    println!("Server has started, listening on: {}\n", addr);
     // tracing::debug!("Listening on: {}", addr);
 
     axum::Server::bind(&addr)
@@ -131,41 +152,13 @@ async fn hello_world() -> &'static str {
 }
 
 async fn ws_handler(ws: WebSocketUpgrade, State(state): State<Arc<AppState>>) -> Response {
-    // ws.on_upgrade(move |socket| handle_socket(socket, addr))
     ws.on_upgrade(|socket| handle_socket(socket, state))
 }
 
-async fn handle_socket(mut socket: WebSocket, state: Arc<AppState>) {
-    // By splitting, we can send and receive at the same time.
+async fn handle_socket(socket: WebSocket, state: Arc<AppState>) {
     let (mut sender, mut receiver) = socket.split();
 
-    // Username gets set in the receive loop, if it's valid.
-    // let mut username = String::new();
-
-    // while let Some(Ok(message)) = receiver.next().await {
-    //     if let Message::Text(name) = message {
-    //         check_username(&state, &mut username, &name);
-    //
-    //         // If not empty we want to quit the loop else we want to quit function.
-    //         if !username.is_empty() {
-    //             break;
-    //         } else {
-    //             // Only send our client that username is taken.
-    //             let _ = sender
-    //                 .send(Message::Text(String::from("Username already taken.")))
-    //                 .await;
-    //
-    //             return;
-    //         }
-    //     }
-    // }
-
     let mut rx = state.tx.subscribe();
-
-    // Now send the "joined" message to all subscribers.
-    // let msg = format!("{} joined.", username);
-    // tracing::debug!("{}", msg);
-    // let _ = state.tx.send(msg);
 
     // Spawn the first task that will receive broadcast messages and send text
     // messages over the websocket to our client.
@@ -178,44 +171,18 @@ async fn handle_socket(mut socket: WebSocket, state: Arc<AppState>) {
     });
 
     let tx = state.tx.clone();
-    // let name = username.clone();
 
-    // Spawn a task that takes messages from the websocket, prepends the user
-    // name, and sends them to all broadcast subscribers.
+    // Spawn a task that takes messages from the websocket and sends them to all broadcast subscribers.
     let mut recv_task = tokio::spawn(async move {
         while let Some(Ok(Message::Text(text))) = receiver.next().await {
-            // Add username before message.
             let _ = tx.send(format!("User: {}", text));
         }
     });
 
-    // If any one of the tasks run to completion, we abort the other.
     tokio::select! {
         _ = (&mut send_task) => recv_task.abort(),
         _ = (&mut recv_task) => send_task.abort(),
     };
-
-    // Send "user left" message (similar to "joined" above).
-    // let msg = format!("{} left.", username);
-    // tracing::debug!("{}", msg);
-    // let _ = state.tx.send(msg);
-
-    // while let Some(msg) = socket.recv().await {
-    //     let msg = if let Ok(msg) = msg {
-    //         println!("{msg:?}");
-    //         msg
-    //     } else {
-    //         println!("Client disconnected!");
-    //         // client disconnected
-    //         return;
-    //     };
-    //
-    //     if socket.send(msg).await.is_err() {
-    //         println!("Client disconnected!");
-    //         // client disconnected
-    //         return;
-    //     }
-    // }
 }
 
 fn check_username(state: &AppState, new_username: &mut String, name: &str) {
@@ -227,60 +194,3 @@ fn check_username(state: &AppState, new_username: &mut String, name: &str) {
         new_username.push_str(name);
     }
 }
-
-// async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
-//     if socket.send(Message::Ping(vec![1, 2, 3])).await.is_ok() {
-//         println!("Pinged {who}...");
-//     } else {
-//         println!("Could not send ping {who}.");
-//
-//         return;
-//     }
-//
-//     if let Some(msg) = socket.recv().await {
-//         if let Ok(msg) = msg {
-//             if process_message(msg, who).is_break() {
-//                 return;
-//             }
-//         } else {
-//             println!("Client {who} disconnected!");
-//
-//             return;
-//         }
-//     }
-// }
-//
-// // NOTE: Copy pasted
-// /// helper to print contents of messages to stdout. Has special treatment for Close.
-// fn process_message(msg: Message, who: SocketAddr) -> ControlFlow<(), ()> {
-//     match msg {
-//         Message::Text(t) => {
-//             println!(">>> {} sent str: {:?}", who, t);
-//         }
-//         Message::Binary(d) => {
-//             println!(">>> {} sent {} bytes: {:?}", who, d.len(), d);
-//         }
-//         Message::Close(c) => {
-//             if let Some(cf) = c {
-//                 println!(
-//                     ">>> {} sent close with code {} and reason `{}`",
-//                     who, cf.code, cf.reason
-//                 );
-//             } else {
-//                 println!(">>> {} somehow sent close message without CloseFrame", who);
-//             }
-//             return ControlFlow::Break(());
-//         }
-//
-//         Message::Pong(v) => {
-//             println!(">>> {} sent pong with {:?}", who, v);
-//         }
-//         // You should never need to manually handle Message::Ping, as axum's websocket library
-//         // will do so for you automagically by replying with Pong and copying the v according to
-//         // spec. But if you need the contents of the pings you can see them here.
-//         Message::Ping(v) => {
-//             println!(">>> {} sent ping with {:?}", who, v);
-//         }
-//     }
-//     ControlFlow::Continue(())
-// }
--- src/web/score.rs
+++ src/web/score.rs
@@ -1,4 +1,4 @@
-use axum::extract::Path;
+use axum::extract::{Path, Query};
 use axum::response::Result;
 use axum::{extract::State, http::StatusCode, Json};
 use serde::{Deserialize, Serialize};
@@ -47,9 +47,17 @@ pub async fn submit_score(
     Ok(Json(score))
 }
 
+#[derive(Debug, Deserialize)]
+pub struct CandidateScoreQuery {
+    event_id: String,
+    category_id: String,
+}
+
+// NOTE: Will only get the final score for ONE category only, maybe loop on the client side?
 pub async fn get_candidate_scores(
     State(pool): State<PgPool>,
     Path(candidate_id): Path<i32>,
+    Query(query): Query<CandidateScoreQuery>,
 ) -> Result<Json<Vec<CandidateScore>>> {
     let q = "SELECT * FROM scores WHERE candidate_id = ($1)";
 
@@ -78,27 +86,66 @@ pub async fn get_candidate_scores(
         })
         .collect();
 
-    let total_criteria_score = add_criteria_scores(State(pool), Path(candidate_id))
+    let final_category_score = get_category_score(State(pool), Path(candidate_id), Query(query))
         .await
         .expect("Failed to add criteria scores.");
 
-    println!("Total criteria score of candidate {candidate_id}: {total_criteria_score}");
+    println!("Category score: {final_category_score}");
 
     Ok(Json(candidate_scores))
 }
 
 // NOTE:
 // Formula: Summation of total category score * weight
-pub async fn add_criteria_scores(
+pub async fn get_category_score(
     State(pool): State<PgPool>,
     Path(candidate_id): Path<i32>,
-) -> Result<i32> {
-    // Get the scores for each criteria on each categorty
-    let q = "SELECT score FROM scores WHERE candidate_id = ($1)";
+    Query(category_query): Query<CandidateScoreQuery>,
+) -> Result<f32> {
+    // Get the number of categories for the event
+    // NOTE: Is this needed?
+    let category_count_q =
+        "SELECT COUNT(*) AS category_count FROM categories WHERE event_id = ($1)";
 
-    let rows = sqlx::query(q)
+    let category_count_row = sqlx::query(category_count_q)
+        .bind(&category_query.event_id)
+        .fetch_one(&pool)
+        .await
+        .expect("Failed to fetch category count.");
+
+    let category_count: i64 = category_count_row.get("category_count");
+
+    println!("Category count: {category_count}\n");
+
+    // NOTE: This is the original beginning of this function, but since a score tracker might be
+    // needed, perhaps getting the score for each criteria one-by-one would be useful instead of
+    // waiting before all scores are submitted?
+
+    // Get category weight
+    let weight_q = "SELECT weight from categories WHERE event_id = ($1) AND id = ($2)";
+
+    let row = sqlx::query(weight_q)
+        .bind(&category_query.event_id)
+        .bind(&category_query.category_id)
+        .fetch_one(&pool)
+        .await
+        .expect("Failed to fetch category weight.");
+
+    let category_weight: f32 = row.get("weight");
+
+    // Get the scores for each criteria on each category
+    // NOTE: do we need to display each criteria scores? Removing the last part of this query
+    // (s.criteria_id = ($3)) will make it so that it will just get all the scores from all judges immediately
+    let scores_q = "SELECT s.score, s.judge_id FROM scores s JOIN criterias c ON s.criteria_id = c.id WHERE s.candidate_id = ($1) AND c.category_id = ($2) AND s.criteria_id = ($3)";
+
+    // NOTE: hard coded for now
+    let criteria_id = "criteria1";
+
+    let rows = sqlx::query(scores_q)
         .bind(&candidate_id)
-        .fetch_all(&(pool))
+        .bind(&category_query.category_id)
+        .bind(&criteria_id)
+        .fetch_all(&pool)
         .await
         .expect("Failed to fetch candidate scores on calculate_total_score()");
 
@@ -106,12 +153,24 @@ pub async fn add_criteria_scores(
         .iter()
         .map(|row| {
             let score: i32 = row.get("score");
+            let judge_id: String = row.get("judge_id");
+
+            println!("{judge_id}: {score}");
 
             score
         })
         .collect();
 
-    let total_criteria_score: i32 = criteria_scores.iter().sum();
+    let category_score: i32 = criteria_scores.iter().sum();
 
-    Ok(total_criteria_score)
+    println!("\nTotal score: {category_score}");
+    println!("Category weight: {category_weight}\n");
+
+    let final_category_score = (category_score as f32) * category_weight;
+
+    Ok(final_category_score)
+}
+
+fn calculate_final_score(category_scores: Vec<f32>) -> f32 {
+    category_scores.iter().sum()
 }
